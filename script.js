967072dca7bb3306feb3c65a96b2288f218c9636
Tsuruki Oga <oga@modeless.jp>
2023-05-09 02:11:55 +0900

    change cell color ,style

--- script.js
+++ script.js
@@ -47,44 +47,51 @@ function getNextColor(currentColor) {
     const index = colors.indexOf(currentColor);
     return colors[(index + 1) % colors.length];
 }
-
-function changeColor(event) {
-    event.target.style.backgroundColor = getNextColor(event.target.style.backgroundColor);
+function changeCellColor(cell) {
+    const currentColor = cell.style.backgroundColor;
+    switch (currentColor) {
+        case 'rgba(255, 99, 97, 0.75)': // pastel red
+            cell.style.backgroundColor = 'rgba(255, 170, 97, 0.75)'; // pastel orange
+            break;
+        case 'rgba(255, 170, 97, 0.75)': // pastel orange
+            cell.style.backgroundColor = 'rgba(152, 251, 152, 0.75)'; // pastel green
+            break;
+        case 'rgba(152, 251, 152, 0.75)': // pastel green
+            cell.style.backgroundColor = 'rgba(135, 206, 250, 0.75)'; // pastel blue
+            break;
+        case 'rgba(135, 206, 250, 0.75)': // pastel blue
+            cell.style.backgroundColor = 'rgba(255, 255, 255, 0.75)'; // white
+            break;
+        case 'rgba(255, 255, 255, 0.75)': // white
+        default:
+            cell.style.backgroundColor = 'rgba(255, 99, 97, 0.75)'; // pastel red
+    }
 }
 
 function createTable() {
-    const table = document.createElement('table');
     const labels = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'];
-
-    // Create header row
-    const headerRow = document.createElement('tr');
-    const emptyHeader = document.createElement('th');
-    headerRow.appendChild(emptyHeader);
-
-    for (const label of labels) {
-        const th = document.createElement('th');
-        th.textContent = label;
-        headerRow.appendChild(th);
-    }
-    table.appendChild(headerRow);
-
-    // Create rows with labels and cells
-    for (const rowLabel of labels) {
-        const tr = document.createElement('tr');
-        const labelCell = document.createElement('th');
-        labelCell.textContent = rowLabel;
-        tr.appendChild(labelCell);
-
-        for (let j = 0; j < 13; j++) {
-            const td = document.createElement('td');
-            td.addEventListener('click', changeColor);
-            tr.appendChild(td);
+    const table = document.createElement('table');
+    for (let i = 0; i <= 13; i++) {
+        const row = document.createElement('tr');
+        for (let j = 0; j <= 13; j++) {
+            const cell = document.createElement('td');
+            if (i > 0 && j > 0) {
+                const stronger = labels[Math.min(i, j) - 1];
+                const weaker = labels[Math.max(i, j) - 1];
+                cell.textContent = stronger + weaker + (i < j ? 's' : i > j ? 'o' : '');
+            } else {
+                cell.textContent = labels[i === 0 ? j - 1 : i - 1];
+            }
+            row.appendChild(cell);
+            if (i !== 0 && j !== 0) {
+                cell.addEventListener('click', () => changeCellColor(cell));
+            }
         }
-        table.appendChild(tr);
+        table.appendChild(row);
     }
-
     return table;
 }
+
 document.addEventListener('DOMContentLoaded', () => {
     const tableContainer = document.getElementById('table-container');
     tableContainer.appendChild(createTable());
